2d6ec9cd63d11400ef040b2315d6090e66af1102
zhanglibin <1058554775@qq.com>
2015-08-28 18:17:35 +0800

    引入fosUserBundle

--- src/Dream/WebBundle/Controller/DefaultController.php
+++ src/Dream/WebBundle/Controller/DefaultController.php
@@ -8,6 +8,13 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+    	$userManager = $this->container->get('fos_user.user_manager');
+    	$user = $userManager->createUser();
+    	$user->setUsername('John');
+    	$user->setPassword('123456');
+		$user->setEmail('john.doe@example.com');
+
+		$userManager->updateUser($user);
         return $this->render('DreamWebBundle:Default:index.html.twig');
     }
 }
